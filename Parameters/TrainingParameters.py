065ff5ecc914d84f8307766a9e6fe4d8daf01f02
Chihcheng Hsieh <37566901+ChihchengHsieh@users.noreply.github.com>
2021-04-12 03:17:02 +1000

    model now can load and resume training (with new parameters <Except optimization>).

--- Parameters/TrainingParameters.py
+++ Parameters/TrainingParameters.py
@@ -36,20 +36,18 @@ class TrainingParameters(object):
     Storing the parameters for controlling the training.
     '''
 
-    # Move it to dataset path class
     bpi_2012_path: str = '../Data/event_logs/BPI_Challenge_2012.xes'
     preprocessed_bpi_2012_folder_path = '../Data/preprocessed/BPI_Challenge_2012'
     preprocessed_df_type: PreprocessedDfType = PreprocessedDfType.HDF5
 
-    load_model_folder_path: str = "SavedModels/2021-04-12 03:06:47.329664" # Set to None to not loading pre-trained model.
-    # load_model_folder_path: str = None # Set to None to not loading pre-trained model.
-
+    # load_model_folder_path: str = "SavedModels/2021-04-12 03:06:47.329664" # Set to None to not loading pre-trained model.
+    load_model_folder_path: str = None # Set to None to not loading pre-trained model.
 
     dataset: SelectableDatasets = SelectableDatasets.BPI2012
     model: SelectableModels = SelectableModels.BaseLineLSTMModel
     optimizer: SelectableOptimizer = SelectableOptimizer.Adam
     loss: SelectableLoss = SelectableLoss.CrossEntropy
-    stop_epoch: int = 2
+    stop_epoch: int = 1
     batch_size: int = 32
     train_test_split_portion = [0.8, 0.1] # Remaining will be used for validation.
     verbose_freq: int = 20
